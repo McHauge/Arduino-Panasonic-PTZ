0c6152720d9d846ac6e7a742541000de9aac81a5
Andreas Thomsen <mc-hauge@hotmail.com>
2020-04-29 05:18:08 +0200

    enter

---
+++ Software/Panazonic-PTZ/src/main.cpp
@@ -0,0 +1,35 @@
+/** 
+ * @Author: McHauge 
+ * @Date: 2020-04-29 05:03:25 
+ * @Desc: Main Goal: Provide a way to directly control of a Panasonic PTZ Head with an Arduino and an X+Y encoder
+ */
+
+/** 
+ * @Author: McHauge 
+ * @Date: 2020-04-29 05:06:02 
+ * @Desc: List of all includes 
+ */
+#include "Arduino.h"
+
+
+
+/** 
+ * @Author: McHauge 
+ * @Date: 2020-04-29 05:05:16 
+ * @Desc: Primarry Setup: 
+ */
+void setup() {
+
+}
+
+
+
+/** 
+ * @Author: McHauge 
+ * @Date: 2020-04-29 05:05:31 
+ * @Desc: Primary Loop 
+ */
+void loop() {
+	
+}
+
